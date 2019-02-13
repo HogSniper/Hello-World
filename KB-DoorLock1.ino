55f0b56873ea89c58ec89e68202934929afc9207
HogSniper <47595757+HogSniper@users.noreply.github.com>
2019-02-13 05:40:59 -0600

    Create KB-DoorLock1.ino

---
+++ KB-DoorLock1.ino
@@ -0,0 +1,119 @@
+/**************************************************************************/
+/* I found this file that closely reflects my goal of RFID Door Lock. 
+I just started this hobby with a Nano gifted to me. 
+Then I put it on a Nano Shield and added an RFID Shield PN532.
+Hat's off to ADAFRUIT! */
+/*! 
+    @file     readMifare.pde
+    @author   Adafruit Industries
+	@license  BSD (see license.txt)
+    This example will wait for any ISO14443A card or tag, and
+    depending on the size of the UID will attempt to read from it.
+   
+    If the card has a 4-byte UID it is probably a Mifare
+    Classic card, and the following steps are taken:
+   
+    - Authenticate block 4 (the first block of Sector 1) using
+      the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
+    - If authentication succeeds, we can then read any of the
+      4 blocks in that sector (though only block 4 is read here)
+	 
+    If the card has a 7-byte UID it is probably a Mifare
+    Ultralight card, and the 4 byte pages can be read directly.
+    Page 4 is read by default since this is the first 'general-
+    purpose' page on the tags.
+This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
+This library works with the Adafruit NFC breakout 
+  ----> https://www.adafruit.com/products/364
+ 
+Check out the links above for our tutorials and wiring diagrams 
+These chips use SPI to communicate, 4 required to interface
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+*/
+/**************************************************************************/
+
+#include <Adafruit_PN532.h>
+
+#define SCK  (2)
+#define MOSI (3)
+#define SS   (4)
+#define MISO (5)
+#define DOOR (12)
+#define LED (11)
+
+Adafruit_PN532 nfc(SCK, MISO, MOSI, SS);
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println("Hello!");
+
+  // pin setup
+  pinMode(LED, OUTPUT);
+  pinMode(DOOR, OUTPUT);
+  digitalWrite(LED, LOW);
+  digitalWrite(DOOR, LOW);
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+  
+  Serial.println("Waiting for an ISO14443A Card ...");
+}
+
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  
+  if(success) {
+    
+    // out uid
+    Serial.print("Detected UID:");
+    for(int i=0; i < uidLength; i++) {
+      Serial.print(" ");
+      Serial.print(uid[i]);
+    }
+    Serial.println("");
+
+    // unlock the door
+    digitalWrite(DOOR, HIGH);
+    
+    // blink LED
+    for(int i=0; i<5; i = i+1) {
+      digitalWrite(LED, LOW);
+      delay(100);    
+      digitalWrite(LED, HIGH);
+      delay(100);
+    }
+
+    // wait another five seconds before re-entering the loop
+    delay(4000);
+    
+    // turn led off
+    digitalWrite(LED, LOW);
+    
+    // lock door
+    digitalWrite(DOOR, LOW);    
+
+  }
+    
+}
